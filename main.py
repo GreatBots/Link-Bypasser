b34395320bae4350772054add3d3f0c471d785e2
Vivek <vivektp2006@gmail.com>
2024-01-21 22:21:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,109 @@
-import asyncio
-from typing import Union
-import logging
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+import pyrogram
+from pyrogram import Client,filters
+from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 from os import environ, remove
 from threading import Thread
 from json import load
 from re import search
-from time import time
-import pyrogram
+
+from texts import HELP_TEXT
 import bypasser
 import freewall
+from time import time
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 # bot
-with open('config.json', 'r') as f:
-    DATA = load(f)
-
-
-def getenv(var):
-    return environ.get(var) or DATA.get(var, None)
-
+with open('config.json', 'r') as f: DATA = load(f)
+def getenv(var): return environ.get(var) or DATA.get(var, None)
 
 bot_token = getenv("TOKEN")
-api_hash = getenv("HASH")
+api_hash = getenv("HASH") 
 api_id = getenv("ID")
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
 
+
+async def get_invite_link(bot: Client, chat_id: Union[str, int]):
+    try:
+        invite_link = await bot.create_chat_invite_link(chat_id=chat_id)
+        return invite_link
+    except FloodWait as e:
+        logger.warning(f"Sleep of {e.value}s caused by FloodWait ...")
+        await asyncio.sleep(e.value)
+        return await get_invite_link(bot, chat_id)
+
+async def handle_force_sub(bot: Client, cmd: Message):
+    try:
+        # Define your channel ID or username here
+        channel_chat_id = -1002092798390  # Replace with your actual channel ID (use negative value for private channels)
+        
+        user = await bot.get_chat_member(chat_id=channel_chat_id, user_id=cmd.from_user.id)
+        
+        if user.status == "kicked":
+            await bot.send_message(
+                chat_id=cmd.from_user.id,
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Chat](https://t.me/schat_ibot).",
+                disable_web_page_preview=True
+            )
+            return 400
+    except UserNotParticipant:
+        try:
+            invite_link = await get_invite_link(bot, chat_id=channel_chat_id)
+        except Exception as err:
+            logger.error(f"Unable to do Force Subscribe to {channel_chat_id}\n\nError: {err}")
+            return 200
+
+        # Notify user to join the channel
+        await bot.send_message(
+            chat_id=cmd.from_user.id,
+            text="**Please Join My Updates Channel to use this Bot!**\n\n"
+                 "Due to Overload, Only Channel Subscribers can use the Bot!",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                    ]
+                ]
+            )
+        )
+        return 400
+    except Exception as e:
+        logger.error(f"Something went wrong: {e}")
+        await bot.send_message(
+            chat_id=cmd.from_user.id,
+            text="Something went Wrong. Contact my [Support Group](https://t.me/JoinOT).",
+            disable_web_page_preview=True
+        )
+        return 200
+    return 200
 
 # handle ineex
-def handleIndex(ele, message, msg):
+def handleIndex(ele,message,msg):
     result = bypasser.scrapeIndex(ele)
-    try:
-        app.delete_messages(message.chat.id, msg.id)
-    except:
-        pass
-    for page in result:
-        app.send_message(message.chat.id, page, reply_to_message_id=message.id, disable_web_page_preview=True)
+    try: app.delete_messages(message.chat.id, msg.id)
+    except: pass
+    for page in result: app.send_message(message.chat.id, page, reply_to_message_id=message.id, disable_web_page_preview=True)
 
 
 # loop thread
-def loopthread(message, otherss=False):
-    urls = []
-    if otherss:
-        texts = message.caption
-    else:
-        texts = message.text
+def loopthread(message,otherss=False):
 
-    if texts in [None, ""]:
-        return
+    urls = []
+    if otherss: texts = message.caption
+    else: texts = message.text
+
+    if texts in [None,""]: return
     for ele in texts.split():
         if "http://" in ele or "https://" in ele:
             urls.append(ele)
-    if len(urls) == 0:
-        return
+    if len(urls) == 0: return
 
-    if bypasser.ispresent(bypasser.ddl.ddllist, urls[0]):
+    if bypasser.ispresent(bypasser.ddl.ddllist,urls[0]):
         msg = app.send_message(message.chat.id, "⚡ __generating...__", reply_to_message_id=message.id)
     elif freewall.pass_paywall(urls[0], check=True):
         msg = app.send_message(message.chat.id, "🕴️ __jumping the wall...__", reply_to_message_id=message.id)
     else:
         if "https://olamovies" in urls[0] or "https://psa.wf/" in urls[0]:
-            msg = app.send_message(message.chat.id, "⏳ __this might take some time...__",
-                                   reply_to_message_id=message.id)
+            msg = app.send_message(message.chat.id, "⏳ __this might take some time...__", reply_to_message_id=message.id)
         else:
             msg = app.send_message(message.chat.id, "🔎 __bypassing...__", reply_to_message_id=message.id)
 
@@ -73,45 +112,37 @@ def loopthread(message, otherss=False):
     temp = None
     for ele in urls:
         if search(r"https?:\/\/(?:[\w.-]+)?\.\w+\/\d+:", ele):
-            handleIndex(ele, message, msg)
+            handleIndex(ele,message,msg)
             return
-        elif bypasser.ispresent(bypasser.ddl.ddllist, ele):
-            try:
-                temp = bypasser.ddl.direct_link_generator(ele)
-            except Exception as e:
-                temp = "**Error**: " + str(e)
+        elif bypasser.ispresent(bypasser.ddl.ddllist,ele):
+            try: temp = bypasser.ddl.direct_link_generator(ele)
+            except Exception as e: temp = "**Error**: " + str(e)
         elif freewall.pass_paywall(ele, check=True):
             freefile = freewall.pass_paywall(ele)
             if freefile:
-                try:
+                try: 
                     app.send_document(message.chat.id, freefile, reply_to_message_id=message.id)
                     remove(freefile)
-                    app.delete_messages(message.chat.id, [msg.id])
+                    app.delete_messages(message.chat.id,[msg.id])
                     return
-                except:
-                    pass
-            else:
-                app.send_message(message.chat.id, "__Failed to Jump", reply_to_message_id=message.id)
-        else:
-            try:
-                temp = bypasser.shortners(ele)
-            except Exception as e:
-                temp = "**Error**: " + str(e)
-        print("bypassed:", temp)
-        if temp != None:
-            links = links + temp + "\n"
+                except: pass
+            else: app.send_message(message.chat.id, "__Failed to Jump", reply_to_message_id=message.id)
+        else:    
+            try: temp = bypasser.shortners(ele)
+            except Exception as e: temp = "**Error**: " + str(e)
+        print("bypassed:",temp)
+        if temp != None: links = links + temp + "\n"
     end = time()
-    print("Took " + "{:.2f}".format(end - strt) + "sec")
+    print("Took " + "{:.2f}".format(end-strt) + "sec")
 
     if otherss:
         try:
             app.send_photo(message.chat.id, message.photo.file_id, f'__{links}__', reply_to_message_id=message.id)
-            app.delete_messages(message.chat.id, [msg.id])
+            app.delete_messages(message.chat.id,[msg.id])
             return
-        except:
-            pass
+        except: pass
 
-    try:
+    try: 
         final = []
         tmp = ""
         for ele in links.split("\n"):
@@ -123,22 +154,21 @@ def loopthread(message, otherss=False):
         app.delete_messages(message.chat.id, msg.id)
         tmsgid = message.id
         for ele in final:
-            tmsg = app.send_message(message.chat.id, f'__{ele}__', reply_to_message_id=tmsgid,
-                                    disable_web_page_preview=True)
+            tmsg = app.send_message(message.chat.id, f'__{ele}__',reply_to_message_id=tmsgid, disable_web_page_preview=True)
             tmsgid = tmsg.id
     except Exception as e:
         app.send_message(message.chat.id, f"__Failed to Bypass : {e}__", reply_to_message_id=message.id)
+        
 
 
 # start command
 @app.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    app.send_message(message.chat.id,
-                     f"👋🏻 Hi **{message.from_user.mention}**, __i am Link Bypasser Bot,\n\njust send me any supported links and i will you get you results.\n\nCheckout /help to Read More__",
-                     reply_markup=InlineKeyboardMarkup([
-                         [InlineKeyboardButton("📣 Updates", url="https://better_botz.t.me")],
-                         [InlineKeyboardButton("Dev", url="https://anocy.t.me")]]),
-                     reply_to_message_id=message.id)
+    app.send_message(message.chat.id, f"👋🏻 Hi **{message.from_user.mention}**, __i am Link Bypasser Bot,\n\njust send me any supported links and i will you get you results.\n\nCheckout /help to Read More__",
+    reply_markup=InlineKeyboardMarkup([
+        [ InlineKeyboardButton("📣 Updates", url="https://better_botz.t.me")],
+        [ InlineKeyboardButton("Dev", url="https://anocy.t.me") ]]), 
+        reply_to_message_id=message.id)
 
 
 # help command
@@ -150,7 +180,7 @@ def send_help(client: pyrogram.client.Client, message: pyrogram.types.messages_a
 # links
 @app.on_message(filters.text)
 def receive(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    bypass = Thread(target=lambda: loopthread(message), daemon=True)
+    bypass = Thread(target=lambda:loopthread(message),daemon=True)
     bypass.start()
 
 
@@ -159,24 +189,24 @@ def docthread(message):
     msg = app.send_message(message.chat.id, "🔎 __bypassing...__", reply_to_message_id=message.id)
     print("sent DLC file")
     file = app.download_media(message)
-    dlccont = open(file, "r").read()
+    dlccont = open(file,"r").read()
     links = bypasser.getlinks(dlccont)
     app.edit_message_text(message.chat.id, msg.id, f'__{links}__', disable_web_page_preview=True)
     remove(file)
 
 
 # files
-@app.on_message([filters.document, filters.photo, filters.video])
+@app.on_message([filters.document,filters.photo,filters.video])
 def docfile(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    
     try:
         if message.document.file_name.endswith("dlc"):
-            bypass = Thread(target=lambda: docthread(message), daemon=True)
+            bypass = Thread(target=lambda:docthread(message),daemon=True)
             bypass.start()
             return
-    except:
-        pass
+    except: pass
 
-    bypass = Thread(target=lambda: loopthread(message, True), daemon=True)
+    bypass = Thread(target=lambda:loopthread(message,True),daemon=True)
     bypass.start()
 
 
